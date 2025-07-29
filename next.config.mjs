edfd043b890d3154686ee73023b8d08086627a2f
Shashwat Mahajan <shashwatmahajan2@gmail.com>
2025-07-29 23:29:47 +0530

    Final Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // This enables static export
+  images: {
+    unoptimized: true, // Required if you're using next/image
+  },
+  trailingSlash: true, // GitHub Pages needs this for correct routing
+  basePath: "/Stock_Dashboard", // Replace with your repo name
+};
 
-export default nextConfig;
+module.exports = nextConfig;
